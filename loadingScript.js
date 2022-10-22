7250060d3f3e2270b29964802f0369072fbe0b62
张计科 <78347874+jeek-zhang@users.noreply.github.com>
2022-10-22 17:13:23 +0800

    Update loadingScript.js

--- loadingScript.js
+++ loadingScript.js
@@ -27,7 +27,7 @@
     }
     // 加载所有js文件
     var jsFiles = [
-        "https://www.jq22.com/jquery/jquery-1.10.2.js",
+        "jquery-1.10.2.min.js",
         "lib/getTime.js",
         "lib/createTip.js",
         "lib/createSpeed.js"
